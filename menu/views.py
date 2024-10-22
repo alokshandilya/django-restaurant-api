968cd3cb4dfe0ad62166286aa8ab1e31079fbaa2
alokshandilya <alok.shandilya10@gmail.com>
2024-10-22 12:04:58 +0530

    chore: update menu index view to return JSON response
    Update the menu index view to return a JSON response with a list of
    items from the database.
    
    - return JSON response with key: "items" and value: list of dictionaries
      representing items from the database

--- menu/views.py
+++ menu/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import HttpResponse
+from django.http import JsonResponse
+
+from .models import Item
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the menu index.")
+    items = Item.objects.all()
+    # items: QuerySet of Item model instances
+
+    # items.values(): Value QuerySet, list of dictionaries
+    # each dictionary represents a row from the database
+    return JsonResponse({"items": list(items.values())})
